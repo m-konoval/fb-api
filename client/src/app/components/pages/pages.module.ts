d308fb7a81c5f9742dc46e4e62faf3035012c0d8
Konoval <MKonoval@kiev.luxoft.com>
2019-03-19 15:46:41 +0200

    fix material animation

--- client/src/app/components/pages/pages.module.ts
+++ client/src/app/components/pages/pages.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PipesModule } from 'src/app/pipes/pipes.module';
 
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { MatGridListModule } from '@angular/material/grid-list';
-import { PipesModule } from 'src/app/pipes/pipes.module';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { PipesModule } from 'src/app/pipes/pipes.module';
   imports: [
     CommonModule,
     MatGridListModule,
+    MatProgressSpinnerModule,
     PipesModule
   ],
   exports: [
